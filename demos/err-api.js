be370f5ef149fec0e7bab45ce12f390a94d48577
Mark K Cowan <mark@battlesnake.co.uk>
2015-01-20 12:50:04 +0200

    Now-playing appears above Vaata otse

---
+++ demos/err-api.js
@@ -0,0 +1,9 @@
+(function (angular) {
+	'use strict';
+
+	angular.module('demo')
+		.value('ETV_api', 'http://etv.err.ee/api')
+		.value('R2_api', 'http://r2.err.ee/api')
+		;
+
+})(window.angular);
--- demos/err-endpoints.js
+++
@@ -1,9 +0,0 @@
-(function (angular) {
-	'use strict';
-
-	angular.module('demo')
-		.value('ETVEndpoint', 'http://etv.err.ee/api')
-		.value('R2Endpoint', 'http://r2.err.ee/api')
-		;
-
-})(window.angular);
