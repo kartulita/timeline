d2c614f7feaf7798010c4fb58e27108b9ec93ff2
Mark K Cowan <mark@battlesnake.co.uk>
2015-02-04 18:04:32 +0200

    TODAY for title of current day

--- timeline-filters.js
+++ timeline-filters.js
@@ -2,13 +2,27 @@
 	'use strict';
 
 	angular.module('battlesnake.timeline')
+		.factory('timelineStrings', timelineStringsFactory)
 		.filter('timelineDate', timelineDateFilter)
 		.filter('timelineTime', timelineTimeFilter)
 		;
 
-	function timelineDateFilter() {
+	function timelineStringsFactory() {
+		var lang = window.navigator.userLanguage || window.navigator.language || 'en';
+		lang = lang.split('-')[0];
+		var today = {
+			en: 'Today',
+			et: 'Täna',
+			ru: 'Сегодня'
+		};
+		return {
+			today: today[lang] || today.en
+		};
+	}
+
+	function timelineDateFilter(timelineStrings) {
 		return function (when) {
-			return when.format('dddd DD.MM');
+			return when.isSame(moment(), 'day') ? timelineStrings.today : when.format('dddd DD.MM');
 		};
 	}
 
